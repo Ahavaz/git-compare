49db656f25cf00168e214f252073b5be1e822df9
Ahavaz <lucas.cesar.8@hotmail.com>
2018-12-05 23:15:54 -0200

    Add Main component styles

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 60px;
+`
+
+export const Form = styled.form`
+  margin-top: 20px;
+  width: 100%;
+  max-width: 400px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 55px;
+    padding: 0 20px;
+    background: #fff;
+    font-size: 18px;
+    color: #444;
+    border-radius: 3px;
+    border: ${({ hasError }) => (hasError ? '2px solid #f00' : 0)};
+  }
+
+  button {
+    height: 55px;
+    width: 80px;
+    padding: 0 20px;
+    margin-left: 10px;
+    background: #63f5b0;
+    color: #fff;
+    border: 0;
+    font-size: 20px;
+    font-weight: bold;
+    border-radius: 3px;
+
+    &:hover {
+      background: #52d89f;
+    }
+  }
+`
